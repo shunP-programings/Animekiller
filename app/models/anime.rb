37389487bed94b420730aa4de0a36d14a7ccd61e
EC2 Default User <ec2-user@ip-172-31-41-203.ec2.internal>
2019-12-19 05:56:34 +0000

    1218のコミットし忘れ分1219hoshi

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -3,7 +3,9 @@ class Anime < ApplicationRecord
 
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :bads,  dependent: :destroy
   has_many :like_users, through: :likes, source: :user
+  has_many :bad_users, through: :bads, source: :user
   
   def like(user)
     likes.create(user_id:user.id)
@@ -15,5 +17,17 @@ class Anime < ApplicationRecord
   
   def liked?(user)
     like_users.include?(user)
+  end
+  
+  def bad(user)
+    bads.create(user_id:user.id)
+  end
+  
+  def unbad(user)
+    bads.find_by(user_id:user.id).destroy
+  end
+  
+  def bad?(user)
+    bad_users.include?(user)
   end
 end
