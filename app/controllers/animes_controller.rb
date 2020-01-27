7d387d6f27b79fa1911f9616613494b484358ac0
EC2 Default User <ec2-user@ip-172-31-41-203.ec2.internal>
2020-01-27 03:49:29 +0000

    編集画面のエラー修正0127hoshi

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -11,6 +11,8 @@ class AnimesController < ApplicationController
   end
 
   def create
+    #default_scope -> { order(created_at: :asc) }
+    @anime = Anime.all.order(created_at: :asc)
     user_id = User.find_by(uid: session[:login_uid]).id
     @anime = Anime.new(message: params[:anime][:message],title: params[:anime][:title], user_id: user_id)
     #params.require(:user).permit(:image)
@@ -19,6 +21,23 @@ class AnimesController < ApplicationController
     else
       redirect_to animes_path
      #render new_anime_path
+    end
+  end
+  
+  def edit
+    @anime = Anime.find(params[:id])
+    #@user = User.new
+    #render "new"
+  end
+
+  def update
+     @anime = Anime.find(params[:id])
+     @anime.update(message: params[:anime][:message],title: params[:anime][:title], user_id: user_id)
+    if anime.save
+      redirect_to animes_path
+    else
+       render 'edit'
+       #render :edit
     end
   end
   
@@ -32,24 +51,5 @@ class AnimesController < ApplicationController
     def user_params
       params.require(:user).permit(:content, :image)
     end
-  
-  
-  def edit
-    @anime = Anime.find(params[:id])
-    #@user = User.new
-    #render "new"
-  end
-
-  def update
-     @anime = Anime.find(params[:id])
-     @anime.update(message: params[:anime][:message])
-     @anime.update(title: params[:anime][:title])
-    if anime.save
-      redirect_to animes_path
-    else
-       render 'edit'
-       #render :edit
-    end
-  end
 
 end
